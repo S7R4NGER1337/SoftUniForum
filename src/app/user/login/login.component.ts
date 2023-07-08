8f3cf2a437484f60db583631a6af9036b89c5ee8
S7R4NGER3301 <nikolakoew@gmail.com>
2023-07-08 13:29:50 +0300

    making login button work

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor(private userService: UserService){}
+
+  login(email:string, password:string):void {
+
+    // for now data isnt dynamic
+    this.userService.login()
+  }
 }
