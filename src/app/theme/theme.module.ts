6856d99c52e6ac7a59e681e5edf00b901a1a892b
S7R4NGER3301 <nikolakoew@gmail.com>
2023-07-08 12:39:29 +0300

    adding current-theme component to theme module

--- src/app/theme/theme.module.ts
+++ src/app/theme/theme.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewThemeComponent } from './new-theme/new-theme.component';
+import { CurrentThemeComponent } from './current-theme/current-theme.component';
 
 
 
 @NgModule({
   declarations: [
-    NewThemeComponent
+    NewThemeComponent,
+    CurrentThemeComponent
   ],
   imports: [
     CommonModule
